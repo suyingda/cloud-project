75d5eccba2f8c349fbc0b5f02d02ca827474b016
苏英大 <suyd@szhcf.com.cn>
2020-03-29 18:18:52 +0800

    addTypes

---
+++ Types/src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { HelloA } from "../components/Hello";
+console.log(6666666666666)
+ReactDOM.render(
+    <div>123</div>,
+    document.getElementById("root")
+);
