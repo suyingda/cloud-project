a9702b7ff4ae880a2f1f8aa0cb7aaaf91612e9a9
苏英大 <suyd@szhcf.com.cn>
2020-03-31 18:21:35 +0800

    wbCliTs

--- webpackCli/webpack.base.config.js
+++ webpackCli/webpack.base.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
@@ -6,18 +8,56 @@ module.exports = {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js", ".jsx"]
     },
-
+    plugins: [new MiniCssExtractPlugin()],
     module: {
         rules: [
-            {test: /\.js|\.jsx/, use: 'babel-loader'},
-            {test: /\.tsx?$/, loader: "awesome-typescript-loader"},
+            // {test: /\.js|\.jsx/, use: 'babel-loader'},
+            {test: /\.tsx$/, loader: "awesome-typescript-loader"},
             {
-                test: /\.css?$/,
+                test: /\.css$/i,
                 use: [
                     'style-loader',
-                    'css-loader',
+                    // MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'typings-for-css-modules-loader',
+                        options: {
+                            modules: true,
+                            namedExport: true,
+                            camelCase: true,
+                            minimize: true,
+                            localIdentName: "[local]_[hash:base64:5]"
+                        }
+                    },
                 ]
             },
+            /*  {
+                  test: /\.css?$/,
+                  use: [
+                      'style-loader',
+                      'css-loader',
+                  ]
+              },*/
+            /*  {
+                  test: /\.less$/,
+                  use: [
+                      {
+                          loader: 'style-loader',
+                      },
+                      {
+                          loader: 'css-loader',
+                      },
+                      {
+                          loader: 'less-loader',
+                          options: {
+                              lessOptions: {
+                                  strictMath: true,
+                                  noIeCompat: true,
+                              },
+                              module:true
+                          },
+                      },
+                  ],
+              },*/
             //file-loader
             // {enforce: "pre", test: /\.js$/, loader: "source-map-loader"}
         ]
